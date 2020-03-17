fc835b54cd0d7a7b01a74e31329ab0942d85a1f5
Hector Gomez <hector.gomez.varela@mobgen.com>
2020-03-17 13:55:25 +0100

    Build consumer node

--- kafka-consumer/src/domain/use-cases/index.js
+++ kafka-consumer/src/domain/use-cases/index.js
@@ -1,7 +1,5 @@
-const sendMessage = require('./send-message-uc');
 const consumeMessages = require('./consume-messages');
 
 module.exports = {
-  sendMessage,
   consumeMessages,
 };
---
+++ kafka-consumer/src/index.js
@@ -0,0 +1,14 @@
+require('../../kafka-producer/node_modules/dotenv').config();
+
+const {
+  kafka,
+  server,
+} = require('./infrastructure');
+
+const { consumeMessages } = require('./domain/use-cases');
+
+const { ROLE, HTTP_PORT } = process.env;
+
+server.listen(HTTP_PORT || 3000);
+
+(() => consumeMessages(kafka))();
